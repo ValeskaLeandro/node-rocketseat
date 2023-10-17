421dd6935e3d45f68573f280d73da99136ba90fd
Valeska Leandro <valeskaleandro@gmail.com>
2023-10-17 13:39:18 -0300

    :heavy_plus_sign: build: Settings prisma.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
-
+import { PrismaClient } from '@prisma/client'
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Valeska Leandro',
+    email: 'valeskaleandro@gmail.com',
+  },
+})
